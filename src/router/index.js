d8e9904c4fbd45e2a62f38c42b23bbc093a0312b
Hudson <hudson.imwgt@gmail.com>
2022-01-05 14:26:57 -0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Cadastro from '../views/Cadastro.vue'
+import Usuario from '../views/Usuario.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/cadastro',
+    name: 'Cadastro',
+    component: Cadastro
+
+  },
+  {
+    path: '/usuario',
+    name: 'Usuario',
+    component: Usuario
+  },
+  ]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/Usuario.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Informação do Usuario</h1>
+  </div>
+</template>
